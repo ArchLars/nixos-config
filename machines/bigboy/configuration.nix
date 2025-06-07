934207440fcb2fa43821c9793384ac52467b1a06
Lars Øydneskleiv <bumbleberrycar@gmail.com>
2025-06-07 09:56:39 +0200

    Update configuration.nix

--- machines/bigboy/configuration.nix
+++ machines/bigboy/configuration.nix
@@ -40,28 +40,20 @@
     networkmanager.enable = true;
   };
 
-  ##############################################################################
-  # Locale / time
-  ##############################################################################
-  time.timeZone = "Europe/Oslo";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "nb_NO.UTF-8/UTF-8"
-    ];
-    extraLocaleSettings = {
-      LC_MESSAGES    = "en_US.UTF-8";
-      LC_TIME        = "nb_NO.UTF-8";
-      LC_MONETARY    = "nb_NO.UTF-8";
-      LC_PAPER       = "nb_NO.UTF-8";
-      LC_MEASUREMENT = "nb_NO.UTF-8";
-      LC_ADDRESS     = "nb_NO.UTF-8";
-      LC_TELEPHONE   = "nb_NO.UTF-8";
-      LC_NAME        = "nb_NO.UTF-8";
-      LC_NUMERIC     = "nb_NO.UTF-8";
-    };
+##############################################################################
+# Locale / time
+##############################################################################
+time.timeZone = "Europe/Oslo";
+i18n = {
+  defaultLocale = "en_US.UTF-8";
+  supportedLocales = [ "en_US.UTF-8/UTF-8" "nb_NO.UTF-8/UTF-8" ];
+  extraLocaleSettings = lib.genAttrs [
+    "LC_TIME" "LC_MONETARY" "LC_PAPER" "LC_MEASUREMENT" 
+    "LC_ADDRESS" "LC_TELEPHONE" "LC_NAME" "LC_NUMERIC"
+  ] (_: "nb_NO.UTF-8") // {
+    LC_MESSAGES = "en_US.UTF-8";
   };
+};
 
   ##############################################################################
   # X11 / keyboard
